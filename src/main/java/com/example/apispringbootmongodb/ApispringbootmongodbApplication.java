f773a4e6e37bae56cf7bcd41617e0216e68936a6
robertoamorim7 <robertovva@gmail.com>
2022-12-28 21:09:01 -0400

    first commit

---
+++ src/main/java/com/example/apispringbootmongodb/ApispringbootmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.apispringbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApispringbootmongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApispringbootmongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apispringbootmongodb/ApispringbootmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apispringbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApispringbootmongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
